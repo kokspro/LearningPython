2f204554b56ff83f94e0785ff20b7f76db35e5b5
Kokspro <98177061+kokspro@users.noreply.github.com>
2025-06-22 19:09:19 -0500

    Almost done with loop syntax

--- learn.py
+++ learn.py
@@ -1,7 +1,22 @@
 # print("Hello World!\n" * 3)
 
-first = "Will"
-last = "Koks"
-full = f"{first} {last}"
-print(full)
+# first = "Will"
+# last = "Koks"
+# full = f"{first} {last}"
+# print(full)
 
+# for number in range(3):
+#     print(f"{number} {full}")
+#     print("Testing", (number + 1) * "." )
+
+# success = False
+# for number in range(3):
+#     print("Attempting...")
+#     if success:
+#         print("Success!")
+#         break
+# else:
+#     print("Attempted 3 times but failed")
+
+# for x in "Oh wow, this is cool":
+#     print(x)
