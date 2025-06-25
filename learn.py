8fe930290c6495d5002f1ed42028bb6f1620f6cc
Kokspro <98177061+kokspro@users.noreply.github.com>
2025-06-24 20:11:22 -0500

    Starting Data Structures

--- learn.py
+++ learn.py
@@ -65,16 +65,16 @@
 # save_user(id = 1, name = "William", age = 39)
 
 # Exercise
-def fizz_buzz(input):
-    if (input % 3 == 0) and (input % 5 == 0):
-        return "FizzBuzz"
-    if input % 3 == 0:
-        return "Fizz"
-    if input % 5 == 0:
-        return "Buzz"
-    return input
+# def fizz_buzz(input):
+#     if (input % 3 == 0) and (input % 5 == 0):
+#         return "FizzBuzz"
+#     if input % 3 == 0:
+#         return "Fizz"
+#     if input % 5 == 0:
+#         return "Buzz"
+#     return input
 
-print(fizz_buzz(9))
-print(fizz_buzz(10))
-print(fizz_buzz(15))
-print(fizz_buzz(19))
+# print(fizz_buzz(9))
+# print(fizz_buzz(10))
+# print(fizz_buzz(15))
+# print(fizz_buzz(19))
